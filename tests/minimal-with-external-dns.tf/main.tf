9fbe0fd39a576ed7c58a4e1f3a1998e3cbbf8137
John Titus <john.titus@rackspace.com>
2019-04-17 14:09:11 -0500

    Update test cert to valid domain

--- tests/minimal-with-external-dns.tf/main.tf
+++ tests/minimal-with-external-dns.tf/main.tf
@@ -2,10 +2,18 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "random_string" "rstring" {
+  length  = 6
+  lower   = true
+  upper   = false
+  number  = false
+  special = false
+}
+
 module "acm" {
   source = "../../module"
 
-  domain      = "example.com"
+  domain      = "${random_string.rstring.result}.mupo181ve1jco37.net"
   environment = "Production"
 
   custom_tags = {
